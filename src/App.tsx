9757e42c164246a9e3c619060a7c71ee11ade1ce
Jude <jdecroos@avanti.ca>
2025-07-25 15:25:24 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Button } from "@av/components/ui/Button";
+import { ConfigProvider } from "antd";
 
 function App() {
+  const theme = {
+    token: {
+      colorPrimary: "#ff4d4f",
+    },
+  };
   return (
-    <>
+    <ConfigProvider theme={theme}>
       <h1>Payroll</h1>
       <div className="card">
         <Button> Button in Payroll </Button>
       </div>
-    </>
+    </ConfigProvider>
   );
 }
 
